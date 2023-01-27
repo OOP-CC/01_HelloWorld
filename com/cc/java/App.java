654a881dc17ce8eb7fe935aa9a83440b35ca3906
Thirhag <meigraf@hotmail.de>
2023-01-27 13:03:12 +0100

    first_encounter

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,8 +3,8 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-       System.out.println("Hello, World!"); 
-    // System.out.println("12:55, Mittag!");
+        System.out.println("Hello, World!"); 
+        System.out.println("TGIF!, Mittag");
     }
 
     
